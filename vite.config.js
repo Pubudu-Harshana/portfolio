c9f39999f15c7616f342627c7cabfc1bc3669573
Pubudu-Harshana <pubuduhar@gmail.com>
2026-02-04 23:58:36 +0530

    Fix deployment base path

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-<<<<<<< HEAD
-  base  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'
-=======
-  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'
-})
->>>>>>> 76aab3bead4490705919b48a461681b63730fa43
+  base: '/',
+});
